eaa79e3d2dd1b39db2386e49ea6663df23942e3b
geoffrey woodcock <theatomproject@gmail.com>
2019-04-20 18:49:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+const badges = ['Ada', 'Brendan', 'Ali'];
+ 
+function printBadges(badges) {
+    for (let i = 0; i < badges.length; i++) {
+        console.log(`Welcome ${badges[i]} You are employee #i`);
+    }
+ 
+    return badges;
+}
+ 
+printBadges(badges);
